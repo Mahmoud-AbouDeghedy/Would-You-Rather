be03ea828d845f7fa17c4766d4f53ef3511cb4bc
Deghedy <67663696+Mahmoud-AbouDeghedy@users.noreply.github.com>
2021-10-17 02:26:18 +0200

    Add files via upload

---
+++ src/actions/questions.js
@@ -0,0 +1,57 @@
+import { saveQuestion, saveQuestionAnswer } from "../utils/api";
+
+export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS",
+  ADD_QUESTION = "ADD_QUESTION",
+  ANSWER_QUESTION = "ANSWER_QUESTION";
+
+export function receiveQuestions(questions) {
+  return {
+    type: RECEIVE_QUESTIONS,
+    questions,
+  };
+}
+
+function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
+    question,
+  };
+}
+
+export function handleAddQuestion(optionOne, optionTwo) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    return saveQuestion({
+      optionOneText: optionOne,
+      optionTwoText: optionTwo,
+      author: authedUser,
+    })
+      .then((question) => {
+        dispatch(addQuestion(question));
+      })
+      .catch(() => {
+        console.error("Error in adding question");
+        alert("Error saving the question,try again");
+      });
+  };
+}
+
+function answerQuestion(info) {
+  return {
+    type: ANSWER_QUESTION,
+
+    info,
+  };
+}
+
+export function handleAnswerQuestion(info) {
+  return (dispatch) => {
+    dispatch(answerQuestion(info));
+    return saveQuestionAnswer(info)
+      .then()
+      .catch(() => {
+        console.error("Error in answering question");
+        alert("Error saving the question answer,try again");
+      });
+  };
+}
